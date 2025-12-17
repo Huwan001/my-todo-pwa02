19cbf87166976289cb3bfd08b507d8374a6fc98f
Huwan001 <zxcasd951159aaabbb@gmail.com>
2025-12-17 19:08:09 +0800

    bump ios icon cache

--- pages/pages_document.js
+++ pages/pages_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         {/* PWA：讓瀏覽器知道 manifest 在哪裡 */}
         <link rel="manifest" href="/manifest.json" />
-		<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+		<link rel="apple-touch-icon" href="/apple-touch-icon-v2.png" />
         {/* 狀態列、標題列的顏色（手機上會看到） */}
         <meta name="theme-color" content="#38bdf8" />
       </Head>
